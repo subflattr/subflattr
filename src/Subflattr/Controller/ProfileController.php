79d9b5d82d2d864117093bb6e5edbe00cb3601a0
SkaveRat <skaverat@funlynx.de>
2013-09-28 01:51:27 +0200

    404 for user profile

--- src/Subflattr/Controller/ProfileController.php
+++ src/Subflattr/Controller/ProfileController.php
@@ -6,25 +6,36 @@ use Subflattr\Application;
 use Subflattr\Entity\User;
 use Subflattr\Repositories\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 
 class ProfileController
 {
 	public function show(Request $request, Application $app)
 	{
+		$rendervars = [
+			'loggedin' => $app->isLoggedIn()
+		];
+		if($app->isLoggedIn())
+			$rendervars['user'] = $app->getUserData();
+		else
+			$rendervars['oauthlink'] = $app->oauth()->getAuthuri();
+
 
 		/** @var UserRepository $repo */
 		$repo = $app->doctrine()->getRepository('Subflattr\Entity\User');
 		/** @var User $user */
 		$user = $repo->findByNormalizedUsername($request->get('name'));
 
-		$rendervars = [
-			'loggedin' => $app->isLoggedIn(),
+		if(!isset($user))
+			return $app->render('profile/notfound.twig', $rendervars, new Response("User not found", 404));
+
+		$rendervars = array_merge($rendervars, [
 			'name' => $user->getUsername(),
 			'greeting' => $user->getFeed()->getGreeting(),
 			'subheading' => $user->getFeed()->getSubheading(),
 			'description' => $user->getFeed()->getDescription()
-		];
+		]);
 
 		if($app->isLoggedIn())
 			$rendervars['user'] = $app->getUserData();
