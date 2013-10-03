c05dce753fea8e7083bbc52684f3cb70ae37a17c
SkaveRat <skaverat@funlynx.de>
2013-10-03 15:31:31 +0200

    adding image upload of creator image

--- src/Subflattr/Controller/CreatorController.php
+++ src/Subflattr/Controller/CreatorController.php
@@ -4,14 +4,27 @@
 namespace Subflattr\Controller;
 
 
+use Imagine\Image\Box;
+use Imagine\Image\Point;
+use Imagine\Imagick\Imagine;
 use Subflattr\Application;
 use Subflattr\Entity\User;
 use Subflattr\Repositories\UserRepository;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 
 class CreatorController {
 
 	public function create(Request $request, Application $app) {
+
+		/** @var UploadedFile $file */
+		$file = $request->files->get('image');
+		if(isset($file) && ($file->getMimeType() == 'image/jpeg' || $file->getMimeType() == 'image/png')) {
+			$imagine = new Imagine();
+			$image = $imagine->open($file->getRealPath());
+			$image = $image->thumbnail(new Box(1000,1000));
+			$image->save('images/avatars/' . strtolower($app->getUserData()['name']));
+		}
 
 		/** @var UserRepository $repo */
 		$repo = $app->doctrine()->getRepository('\Subflattr\Entity\User');
